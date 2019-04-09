f46f6a638d3d586791c8fece5d71a88c4181b9a2
Francis Indaheng <findaheng@berkeley.edu>
2019-04-08 22:27:54 -0700

    init:

--- ScheduleBuilder.java
+++
@@ -1,5 +0,0 @@
-public class ScheduleBuilder {
-  public static void main(String[] args) {
-    System.out.println("hello world");
-  }
-}
---
+++ schedule/ScheduleBuilder.java
@@ -0,0 +1,16 @@
+package schedule;
+
+import java.util.List;
+
+public class ScheduleBuilder {
+  private List<Class> classes;
+
+  public ScheduleBuilder() {
+
+  }
+
+
+  public static void main(String[] args) {
+
+  }
+}
