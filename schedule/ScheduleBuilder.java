fb12a71d5c7e05d052c9ad6090b3abd304ccbbf5
Francis Indaheng <findaheng@berkeley.edu>
2019-04-10 01:01:28 -0700

    addCourse progress

--- schedule/ScheduleBuilder.java
+++ schedule/ScheduleBuilder.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class ScheduleBuilder {
     String name;
-    List<Semester> semList;
+    List<Semester> semesterList;
 
     public ScheduleBuilder(String name) {
         this.name = name;
@@ -16,7 +16,7 @@ public class ScheduleBuilder {
         Semester sem6 = new Semester(Semesters2022.SP21);
         Semester sem7 = new Semester(Semesters2022.FA21);
         Semester sem8 = new Semester(Semesters2022.SP22);
-        semList = List.of(sem1, sem2, sem3, sem4, sem5, sem6, sem7, sem8);
+        semesterList = List.of(sem1, sem2, sem3, sem4, sem5, sem6, sem7, sem8);
     }
 
     /** Help menu listing available actions.*/
@@ -25,9 +25,40 @@ public class ScheduleBuilder {
                 + "   addCourse(Semester sem, Course cse) — ");
     }
 
-    /** Add a course to a specific semester.*/
-    public void addCourse(Semester sem, Course cse) {
+    /** Returns the corresponding semester enum to the
+     *  inputted String. Format must be as follows:
+     *  "Fall 2018" in which capitalization does
+     *  not matter.
+     */
+    private Semesters2022 stringToSemester(String s) {
+        if (s.equalsIgnoreCase("fall 2018")) {
+            return Semesters2022.FA18;
+        } else if (s.equalsIgnoreCase("spring 2019")) {
+            return Semesters2022.SP19;
+        } else if (s.equalsIgnoreCase("fall 2019")) {
+            return Semesters2022.FA19;
+        } else if (s.equalsIgnoreCase("spring 2020")) {
+            return Semesters2022.SP20;
+        } else if (s.equalsIgnoreCase("fall 2020")) {
+            return Semesters2022.FA20;
+        } else if (s.equalsIgnoreCase("spring 2021")) {
+            return Semesters2022.SP21;
+        } else if (s.equalsIgnoreCase("fall 2021")) {
+            return Semesters2022.FA21;
+        } else if (s.equalsIgnoreCase("spring 2022")) {
+            return Semesters2022.SP22;
+        } else {
+            throw new IllegalArgumentException("Must be one of the follwing: Fall 2018, "
+                    + "Spring 2019, Fall 2019, Spring 2020, Fall 2020, Spring 2021, "
+                    + "Fall 2021, Spring 2020.");
+        }
+    }
 
+    /** Add a course to a specific semester.*/
+    public void addCourse(String s, Department d, String c, String n, int u) {
+        Semesters2022 sEnum = stringToSemester(s);
+        Semester sem = semesterList.get(semesterList.indexOf(sEnum));
+        sem.addCourse(new Course(d, c, n, u));
     }
 
 
